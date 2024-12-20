1b90632db29d95f4e38d7eb83e4089f2b8bd56b0
Edvige Di Salvo <edvigedis@google.com>
2024-12-20 11:27:13 +0100

    nothing changed, I am not going to try and fix the local host right before presentation

--- config/index.js
+++ config/index.js
@@ -11,18 +11,20 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
-module.exports = (app) => {
-  // Configure CORS pour accepter les requêtes de votre frontend
-  app.use(
-    cors({
-      origin: "https://pawty.netlify.app", // Remplacez par l'URL exacte de votre frontend
-      methods: "GET,POST,PUT,DELETE", // Méthodes autorisées
-      allowedHeaders: "Content-Type,Authorization", // En-têtes autorisés
-    })
-  );
+    // "https://pawty.netlify.app", // Production frontend
+    //   "http://localhost:5173", // Development frontend
+      (module.exports = (app) => {
+        // Configure CORS pour accepter les requêtes de votre frontend
+        app.use(
+          cors({
+            origin: "https://pawty.netlify.app", // Remplacez par l'URL exacte de votre frontend
+            methods: "GET,POST,PUT,DELETE", // Méthodes autorisées
+            allowedHeaders: "Content-Type,Authorization", // En-têtes autorisés
+          })
+        );
 
-  // Le reste de votre configuration
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-};
+        // Le reste de votre configuration
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+      });
